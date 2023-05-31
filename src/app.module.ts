c43f075d109a047a1a67864b78228cb57d95d6ee
Jack Miller <jackmiller0899@gmail.com>
2023-05-31 16:03:18 +0700

    Remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { CommissionModule } from './modules/commission/commission.module';
 import { DatabaseModule } from './database/database.module';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 // configs
 import databaseConfiguration from './shared/config/database.configuration';
@@ -15,7 +14,6 @@ import serverConfiguration from './shared/config/server.configuration';
     DatabaseModule,
     CommissionModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
