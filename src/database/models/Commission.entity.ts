3f3ba44df936735b87fbc76bff253a363178c1bb
Jack Miller <jackmiller0899@gmail.com>
2023-05-31 11:02:59 +0700

    Update type to commission relationship

--- src/database/models/Commission.entity.ts
+++ src/database/models/Commission.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { CommissionType } from './ComissionType.entity';
 
 @Entity()
@@ -9,7 +9,7 @@ export class Commission {
   @Column({ type: 'varchar' })
   name: string;
 
-  @OneToOne(() => CommissionType, (type) => type.id)
+  @ManyToOne(() => CommissionType, (type) => type.id)
   @JoinColumn()
   type: CommissionType;
 
