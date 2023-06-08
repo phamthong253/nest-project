8f2878689c66b1b414aca0fe17694ca2b71c7734
Jack Miller <jackmiller0899@gmail.com>
2023-06-08 10:30:52 +0700

    Add get user permission

--- src/modules/security/controllers/auth/auth.controller.ts
+++ src/modules/security/controllers/auth/auth.controller.ts
@@ -1,24 +1,30 @@
-import { Body, Post } from '@nestjs/common';
+import { Body, Get, Post, Req } from '@nestjs/common';
 import { ControllerPrefix } from '../../shared/controller-prefix.enum';
 import { CreateUserDto } from 'src/modules/user/dtos/create-user.dto';
 import { AuthService } from '../../services/auth/auth.service';
 import { SignInDto } from '../../dtos/sign-in.dto';
 import { Public } from '@decorators/public-route.decorator';
 import { Route } from '@decorators/route.decorator';
+import { PermissionService } from '../../services/permission/permission.service';
 
 @Route(ControllerPrefix.AUTH)
 export class AuthController {
-  constructor(private readonly _authService: AuthService) {}
+  constructor(private readonly _authService: AuthService, private readonly _permissionService: PermissionService) {}
+
+  @Get()
+  getUserPermission(@Req() { user }: Record<keyof Request | 'user', any>): Promise<string[]> {
+    return this._permissionService.findByUserId(user.userId);
+  }
 
   @Public()
   @Post('sign-in')
-  signIn(@Body() { username, password }: SignInDto) {
+  signIn(@Body() { username, password }: SignInDto): Promise<{ accessToken: string }> {
     return this._authService.signIn(username, password);
   }
 
   @Public()
   @Post('sign-up')
-  async signUp(@Body() createUserDto: CreateUserDto) {
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<{ accessToken: string }> {
     await this._authService.signUp(createUserDto);
 
     return this._authService.signIn(createUserDto.username, createUserDto.password);
--- src/modules/security/services/permission/permission.service.ts
+++ src/modules/security/services/permission/permission.service.ts
@@ -1,13 +1,34 @@
+import { DataSource, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AppPermission } from '../../shared/permissions.enum';
 import { Permission } from '@models/permission.entity';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { User } from '@models/user.entity';
 
 @Injectable()
 export class PermissionService {
-  constructor(@InjectRepository(Permission) private readonly _permissionRepo: Repository<Permission>) {}
+  constructor(@InjectRepository(Permission) private readonly _permissionRepo: Repository<Permission>, private readonly _dataSource: DataSource) {}
 
   async findAll(): Promise<Permission[]> {
     return await this._permissionRepo.find();
+  }
+
+  async findByUserId(id: string): Promise<string[]> {
+    const permissionNameList: string[] = [];
+    const permissionNames = await this._dataSource
+      .getRepository(User)
+      .createQueryBuilder('user')
+      .select('permission.name', 'permissionName') // Select the permission name
+      .distinctOn(['permission.name'])
+      .innerJoin('user.roles', 'role') // Join the Role entity
+      .innerJoin('role.permissions', 'permission') // Join the Permission entity
+      .where('user.id = :id AND role.enabled = true', { id })
+      .getRawMany<{ permissionName: AppPermission }>();
+
+    for (const { permissionName } of permissionNames) {
+      permissionNameList.push(permissionName);
+    }
+
+    return permissionNameList;
   }
 }
