1027c5029b27d06515668d07e73113a8f7d80d90
Jack Miller <jackmiller0899@gmail.com>
2023-06-26 08:18:08 +0700

    Add support for port 4200

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, { cors: { origin: ['http://localhost:5173'] } });
+  const app = await NestFactory.create(AppModule, { cors: { origin: ['http://localhost:5173', 'http://localhost:4200'] } });
   const configService = app.get(ConfigService);
 
   // OpenAPI Configs
